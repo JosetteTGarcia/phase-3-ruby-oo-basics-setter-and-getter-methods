f594da5d6c61f94c1c246b6075ea12b95495a4a6
Josette Garcia <josettetgarcia@gmail.com>
2022-05-14 11:19:22 -0500

    commit tree 99aeba1c3aa8fbaa4022845ebf64cc5422bfbb63

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
 
-end
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job_title)
+    @job = job_title
+  end
+
+  def job
+    @job
+  end
+end 
